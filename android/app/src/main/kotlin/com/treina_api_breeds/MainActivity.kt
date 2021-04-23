c57554d16c509117a0d0544b442bc19dc9a2790b
Jean Carlos Schappo <jeancsbnu@gmail.com>
2021-04-22 21:30:51 -0300

    Removido pra adicionar as ´pasta dos projetos

--- android/app/src/main/kotlin/com/treina_api_breeds/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.treina_api_breeds
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/main.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_modular/flutter_modular.dart';
-import 'package:treina_api_breeds/pages/module_base/modulo_base_module.dart';
-
-void main() {
-  runApp(ModularApp(module: ModuleBaseModule()));
-}
